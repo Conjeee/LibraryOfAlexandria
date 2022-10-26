bbfceaf4f53dc5403225ab026bab511e8ee00c78
Conjiii <allencabinta@gmail.com>
2022-10-26 14:40:23 +0800

    part 7 LoA

--- app/src/main/java/com/example/libraryofalexandria/MainActivity.java
+++ app/src/main/java/com/example/libraryofalexandria/MainActivity.java
@@ -1,30 +1,62 @@
 package com.example.libraryofalexandria;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.libraryofalexandria.fragment.FollowedFragment;
 import com.example.libraryofalexandria.fragment.HomeFragment;
 import com.example.libraryofalexandria.fragment.PersonalPostsFragment;
 import com.example.libraryofalexandria.fragment.SettingsFragment;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     BottomNavigationView bottomNavigationView;
     FrameLayout frameLayout;
+
+    ImageView userprofile_image;
+
+    FirebaseAuth auth;
+    FirebaseUser user;
+
+    GoogleSignInClient mGoogleSignInClient;
+    private static final int  RC_SIGN_IN = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +70,17 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         frameLayout = findViewById(R.id.frame_layout);
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        userprofile_image = findViewById(R.id.user_profile_image);
+
+        GoogleSignInOptions gsc = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(MainActivity.this, gsc);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -70,6 +113,82 @@ public class MainActivity extends AppCompatActivity {
         });
 
         bottomNavigationView.setSelectedItemId(R.id.home);
+
+        userprofile_image.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                if (user != null) {
+                    Toast.makeText(MainActivity.this, "User is signed in", Toast.LENGTH_SHORT).show();
+                } else {
+                    showDialog();
+                }
+            }
+        });
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setCancelable(true);
+
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+        View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.item_signin_dialogue, viewGroup, false);
+
+        builder.setView(view);
+
+        TextView button_google_signin = view.findViewById(R.id.button_google_signin);
+        button_google_signin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+        builder.create().show();
+    }
+
+    private void signIn() {
+        Intent intent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+                            HashMap<String, Object> map = new HashMap<>();
+                            map.put("username", account.getDisplayName());
+                            map.put("email", account.getEmail());
+                            map.put("profile", String.valueOf(account.getPhotoUrl()));
+                            map.put("uid", firebaseUser.getUid());
+                            map.put("search", account.getDisplayName().toLowerCase());
+
+                            DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("user");
+                            reference.child((firebaseUser.getUid())).setValue(map);
+
+                        } else {
+                            Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+            catch (Exception e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void selectedFragment(Fragment fragment) {
